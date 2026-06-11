2ec9603e38ae0385393ee88da15273680b12e4e6
thiagodellano <thiagodellano689@gmail.com>
2026-06-11 08:46:08 -0300

    Implementa métricas dinâmicas para todos os serviços

--- app.py
+++ app.py
@@ -8,7 +8,19 @@ def home():
     dados = {
         "web_status": "🟢 Online",
         "latencia": 42,
-        "rps": 315
+        "rps": 315,
+
+        "db_status": "🟢 Online",
+        "cpu": 38,
+        "memoria": 62,
+
+        "dns_status": "🟡 Atenção",
+        "dns_tempo": 180,
+
+        "smtp_status": "🔴 Offline",
+        "smtp_falhas": 12
+
+
     }
 
     return render_template(
